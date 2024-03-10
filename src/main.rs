4b1fca992608b9b032aa746bc7bf86f7013f1661
interrrp <int@addictedto.coffee>
2024-03-10 19:44:38 +0800

    Initialize Raylib handle

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use raylib::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let (mut rl, thread) = raylib::init()
+        .size(1280, 720)
+        .title("dvd-rs")
+        .vsync()
+        .build();
+
+    while !rl.window_should_close() {
+        let mut d = rl.begin_drawing(&thread);
+        d.clear_background(Color::BLACK);
+    }
 }
